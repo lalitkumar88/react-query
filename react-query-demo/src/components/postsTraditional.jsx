e0325ff18d4fe23bb2247f1b370eba50667cd770
lalitkumar88 <117524780+lalitkumar88@users.noreply.github.com>
2025-04-01 10:01:30 +0000

    basics files created

---
+++ react-query-demo/src/components/postsTraditional.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function PostsTraditional() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://improved-fishstick-x65wjvp9v752qq4-4000.app.github.dev/posts"
+      );
+      setPosts(response.data);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  if (isLoading) {
+    return <div>Page is loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error has occurred...</div>;
+  }
+
+  return (
+    <div className="post-list">
+      {posts.map((post) => (
+        <div className="post-item" key={post.id}>
+          <h3 className="post-title">{post.title}</h3>
+          <p className="post-body">{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PostsTraditional;
